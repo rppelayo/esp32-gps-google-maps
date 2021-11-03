4506b6350c40f7749ce234ee4c937630330ee21d
Rolando Pelayo <kurimawxx00@gmail.com>
2021-11-03 11:02:29 +0800

    Update gps.php
    Removed isset($_POST), replaced with file_get_contents("php://input"). Added string parsing for input stream.

--- esp32-gmaps-external/server files/gps.php
+++ esp32-gmaps-external/server files/gps.php
@@ -1,8 +1,12 @@
 <?php
-    if(isset($_POST['lat']) && isset($_POST['lng'])){
+    $inp = file_get_contents("php://input");
+    if(!empty($inp)){
+        $s = explode("&",$inp);
+        $a = explode("=",$s[0]);
+        $b = explode("=",$s[1]);
         // Replace this with another method that saves to DB
         $file = fopen("gps_data.txt", "w") or die("Unable to open file!");
-        $txt = $_POST['lat'] . "," . $_POST['lng'];
+        $txt = $a[1] . "," . $b[1];
         fwrite($file, $txt);
         fclose($file);
     }else{
@@ -11,6 +15,4 @@
         echo fread($file,filesize("gps_data.txt"));
         fclose($file); 
     }
-
-
 ?>
